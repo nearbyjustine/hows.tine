87cf00d8658ee9d93c5ca0865a798cf39cdac729
nearbyjustine <nearbyjustine@gmail.com>
2024-10-26 08:06:05 +0800

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,162 @@
+const logotext = 'HOWS.TINE'
+const meta = {
+  title: 'Justine Castaneda',
+  description:
+    'I’m Justine Castaneda Full Stack Marketing Tech Developer, currently working in the Philippines'
+}
+
+const introdata = {
+  title: 'I’m Justine Castaneda',
+  animated: {
+    first: 'I like building stuff',
+    second: 'I design UI and UX',
+    third: 'I develop web applications and systems'
+  },
+  description:
+    "I'm a Full Stack Marketing Tech Developer, currently working in the Philippines",
+  your_img_url: '/me.jpg'
+}
+
+const dataabout = {
+  title: 'a bit about me',
+  aboutme:
+    'As a dedicated developer by profession and a passionate musician at heart, I find joy in harmonizing the intricacies of code with the rhythms of melody. With a keen eye for design and a knack for problem-solving, I delight in crafting elegant solutions that not only function seamlessly but also resonate with creativity and artistry.'
+}
+const worktimeline = [
+  {
+    jobtitle: 'Full Stack Marketing and Web Developer',
+    where: 'Altus Digital',
+    date: '2024'
+  },
+  {
+    jobtitle: 'Full Stack Web Developer',
+    where: 'Savorite | MyDreamVisa Inc.',
+    date: '2023'
+  }
+]
+
+const skills = [
+  {
+    name: '',
+    value: 90
+  }
+]
+
+const services = [
+  {
+    title: 'Web and Systems Development',
+    description:
+      'I understand that your business has unique needs. Our Web and Systems Development services focus on creating customized solutions that fit your vision and help you connect with your audience effectively.'
+  },
+  {
+    title: 'UI/UX Design',
+    description:
+      'Your users deserve an experience that feels intuitive and inviting. Our UI/UX Design services prioritize your audience’s needs, crafting interfaces that are easy to navigate and enjoyable to use.'
+  }
+]
+
+const dataportfolio = [
+  {
+    img: '/edukado.png',
+    description:
+      'eDukado: A learning management system developed for a local elementary school--with QR attendance monitoring, classroom administration and teacher-principal management.',
+    link: 'https://github.com/nearbyjustine/edukado'
+  },
+  {
+    img: '/otms.png',
+    description:
+      'PUPSRC-OTMS: A transaction management system for on-campus transactions in PUP Santa Rosa',
+    link: 'https://github.com/nearbyjustine/PUPSRC-OTMS/tree/dev'
+  }
+  //   {
+  //     img: 'https://picsum.photos/400/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/600/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/300/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/700/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+
+  //   {
+  //     img: 'https://picsum.photos/400/600/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/300/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/550/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   },
+  //   {
+  //     img: 'https://picsum.photos/400/700/?grayscale',
+  //     description:
+  //       'The wisdom of life consists in the elimination of non-essentials.',
+  //     link: '#'
+  //   }
+]
+
+const contactConfig = {
+  YOUR_EMAIL: 'name@domain.com',
+  YOUR_FONE: '(555)123-4567',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: 'service_id',
+  YOUR_TEMPLATE_ID: 'template_id',
+  YOUR_USER_ID: 'user_id'
+}
+
+const socialprofils = {
+  github: 'https://github.com/nearbyjustine',
+  linkedin: 'https://www.linkedin.com/in/nearby-justine/'
+  //   twitter: 'https://twitter.com'
+}
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext
+}
